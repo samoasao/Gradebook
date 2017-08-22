848298c1b2d43d029db34f0771c84f8c5754e00f
Tyler Wright <samoasao@gmail.com>
2017-08-22 11:36:34 -0700

    Saving scores to the database

--- app/controllers/schedule_items_controller.rb
+++ app/controllers/schedule_items_controller.rb
@@ -1,6 +1,24 @@
 class ScheduleItemsController < ApplicationController
 	def create
+		# TODO: IMPLEMENT TO RECORD
 		puts "WOWEE!!!"
-		redirect_to root_path
+
+		@schedule_item = ScheduleItem.new(schedule_item_params)
+
+		puts "Scores " + @schedule_item.scores.last.value.to_s + "!"
+
+	  if @schedule_item.save
+	    redirect_to root_path, notice: 'Scores were successfully created.' 
+	  else
+	  	redirect_to root_path, notice: 'Scores were NOT successfully created.' 
+	  end
+
 	end
+
+	private
+
+		def schedule_item_params
+			params.require(:schedule_item).permit(:day, :subject_id, :time, scores_attributes: [:assignment_id, :value])
+		end
+
 end
